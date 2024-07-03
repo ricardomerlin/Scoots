8811b2b3bd784cced3fba5ec08282de1d3ef15ad
Ricardo Merlin <rcrdmerlin@gmail.com>
2024-07-02 21:05:31 -0400

    removing database, building with sql alchemy instead

--- backend/scootsproject/scootsapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('questionsets/', views.questionset_list, name='questionset_list'),
-    path('questionsets/<int:pk>/', views.questionset_detail, name='questionset_detail'),
-]
