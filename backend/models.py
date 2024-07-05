e5a84497b648995873e8828a0bd94575020e049e
Ricardo Merlin <rcrdmerlin@gmail.com>
2024-07-04 22:58:25 -0400

    adding page for game active

--- backend/models.py
+++ backend/models.py
@@ -11,13 +11,44 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy()
 
 class User(db.Model, SerializerMixin):
-    __tablename__ = 'users'
+    __tablename__ = 'users_table'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     firstname = db.Column(db.String(80), nullable=False)
     lastname = db.Column(db.String(80), nullable=False)
     password = db.Column(db.String(120), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    games = db.relationship('Game', back_populates='players')
     
     def __repr__(self):
         return f'<User {self.username}>'
+    
+class Game(db.Model, SerializerMixin):
+    __tablename__ = 'games_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    creator_id = db.Column(db.Integer, ForeignKey('users_table.id'))
+
+    players = db.relationship('User', back_populates='games')
+    # questions = db.relationship('Question', back_populates='games')
+    
+    def __repr__(self):
+        return f'<Game {self.name}>'
+    
+class Question(db.Model, SerializerMixin):
+    __tablename__ = 'questions_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(80), nullable=False)
+    answer = db.Column(db.String(80), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    games = db.relationship('Game', back_populates='questions')
+
+    def __repr__(self):
+        return f'<Question {self.question}>'
---
+++ frontend/src/GameScreen.js
@@ -0,0 +1,10 @@
+function JoinRoom() {
+
+    return (
+        <div className="game-started-main">
+            <h1>Game Screen</h1>
+        </div>
+    );
+}
+
+export default JoinRoom;
